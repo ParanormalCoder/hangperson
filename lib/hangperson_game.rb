29d45e199ed1c7f3df97d46106451aa9136f3cb9
Arun <arunkumarm1995@gmail.com>
2016-06-20 10:03:27 +0530

    complete game logic and set up controllers

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,9 +7,45 @@ class HangpersonGame
 
   # def initialize()
   # end
-  
+  attr_accessor :word, :guesses, :wrong_guesses, :word_with_guesses, :check_win_or_lose
+
   def initialize(word)
     @word = word
+    @guesses = ""
+    @wrong_guesses = ""
+    @check_win_or_lose = :play
+  end
+
+  def guess(guess_made)
+
+    guess_made.downcase! unless guess_made.nil? #to make the guess case-insensitive
+    raise ArgumentError unless guess_made =~ /^[a-z]$/
+
+    #if guess is already made do nothing
+    if @guesses.include? guess_made or @wrong_guesses.include? guess_made
+      false
+    #otherwise modify guesses and wrong_guesses accordingly
+    else
+      if @word.include? guess_made
+        @guesses += guess_made
+      else
+        @wrong_guesses += guess_made
+      end
+      @word_with_guesses = ""
+
+      #construct the word guessed so far
+      @word.chars do |char|
+        @word_with_guesses += @guesses.include?(char) ? char : "-"
+      end
+
+      #Update the status of the game
+      if @wrong_guesses.length == 7
+        @check_win_or_lose = :lose
+      elsif @word == @word_with_guesses
+        @check_win_or_lose = :win
+      end
+      true
+    end
   end
 
   def self.get_random_word
